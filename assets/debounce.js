59f4421712f6a7d141d47ab9b4b4059d6b820514
Nopke <anopke@gmail.com>
2025-06-15 19:12:40 +0200

    move debounce to a separate js

---
+++ assets/debounce.js
@@ -0,0 +1,9 @@
+function debounce(callback, wait) {
+    let timeoutId = null;
+    return (...args) => {
+        window.clearTimeout(timeoutId);
+        timeoutId = window.setTimeout(
+            () => callback(...args), wait
+        );
+    };
+}
