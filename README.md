5eb3e97a2741df3a75cd3115d8961c61fc632cd4
Nopke <anopke@gmail.com>
2025-06-24 16:27:08 +0200

    * common.js: add function to put placeholders of elements to their titles * tweak description of BHAV to diagram * tweak index page styles

--- README.md
+++ README.md
@@ -3,6 +3,6 @@ https://cosmatevs.github.io/bhav-to-diagram.html
 
 A web tool for The Sims 2 modders that converts [SimPe](https://modthesims.info/showthread.php?t=630456) [BHAV](https://modthesims.info/wiki.php?title=BHAV) code to [Mermaid](https://mermaid.live/edit) diagrams. It's intended to make BHAV functions easier to understand, especially the big ones.
 
-Hover over interface elements (e.g. buttons, text areas) to see explanations of what they do.
+Hover over interface elements (e.g. buttons, icons, text areas) to see explanations of what they do.
 
 ![BHAV to Mermaid diagram tool appearance](readme-images/bhav-to-diagram.png)
--- assets/common.js
+++ assets/common.js
@@ -1,17 +1,23 @@
 function debounce(callback, wait) {
-    let timeoutId = null;
-    return (...args) => {
-        window.clearTimeout(timeoutId);
-        timeoutId = window.setTimeout(
-            () => callback(...args), wait
-        );
-    };
+	let timeoutId = null;
+	return (...args) => {
+		window.clearTimeout(timeoutId);
+		timeoutId = window.setTimeout(
+			() => callback(...args), wait
+		);
+	};
 }
 
 function removeFromArray(array, object) {
-    array.splice(array.indexOf(object), 1);
+	array.splice(array.indexOf(object), 1);
 }
 
 function distinctFilter(value, index, array) {
-    return array.indexOf(value) === index;
+	return array.indexOf(value) === index;
+}
+
+function putPlaceholdersInTitles() {
+	document.querySelectorAll('*[placeholder]').forEach(element => {
+		element.setAttribute('title', element.getAttribute('placeholder'));
+	})
 }
